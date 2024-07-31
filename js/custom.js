9dde0446176ee8c47907b00ed779145aa0f99523
Hamid Rayati <hamid.rayati@gmail.com>
2024-07-31 12:33:38 +0330

    ADD: random images in home section.

--- js/custom.js
+++ js/custom.js
@@ -11,6 +11,38 @@ $(window).load(function () {
 $(document).ready(function () {
     "use strict";
 
+    // Random Home Images
+    var images = [
+        {
+            "src": "img/portfolio/18-neshan-2/neshan-2.jpg",
+            "alt": "neshan-2"
+        },
+        {
+            "src": "img/portfolio/14-lake/lake.jpg",
+            "alt": "lake"
+        },
+        {
+            "src": "img/portfolio/02-garden/garden.jpg",
+            "alt": "garden"
+        },
+        {
+            "src": "img/portfolio/04-narenj/narenj.jpg",
+            "alt": "narenj"
+        },
+        {
+            "src": "img/portfolio/21-neshan-5-shiraz/neshan-5-shiraz.jpg",
+            "alt": "neshan-5-shiraz"
+        },
+    ]
+    const array = [0, 1, 2, 3, 4];
+    const shuffledArray = array.sort(function(a, b) { return 0.5 - Math.random() });
+
+    $('.demo-box-img').each(function(index) {
+        var img = $(this);
+        img.attr('src', images[shuffledArray[index]].src);
+        img.attr('alt', images[shuffledArray[index]].alt);
+    });
+
     // scroll menu
     var sections = $('.section'),
         nav = $('.navbar-fixed-top,footer'),
@@ -34,13 +66,17 @@ $(document).ready(function () {
     });
 
     nav.find('a').on('click', function () {
-        var $el = $(this),
-            id = $el.attr('href');
+        var $el = $(this);
+        var id = $el.attr('href');
 
+        if(!id.startsWith("#")) return true
+
+        // in about section we have negative margin in design.
+        var offset = (id === "#about") ? 200 : 2
         $('html, body').animate({
-            scrollTop: $(id).offset().top - nav_height + 2
+            scrollTop: $(id).offset().top - nav_height + offset
         }, 600);
-
+        
         return false;
     });
 
